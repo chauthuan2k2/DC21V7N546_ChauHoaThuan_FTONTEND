f659bd59ee97ac4474d5c2b1a577053b35fb45ad
thuandc21v7n546@vlvh.ctu.edu.vn <thuandc21v7n546@vlvh.ctu.edu.vn>
2025-10-06 21:10:01 +0700

    Tao lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container d-flex justify-content-center">
+    <div class="col-md-6">
+      <h4 class="mb-3 text-center">
+        Thêm Liên hệ mới <i class="fas fa-user-plus"></i>
+      </h4>
+      <ContactForm :contact="newContact" @submit:contact="addContact" />
+    </div>
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+export default {
+  name: "ContactAdd",
+  components: { ContactForm },
+  data() {
+    return {
+      newContact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+    };
+  },
+  methods: {
+    addContact(contact) {
+      console.log("Liên hệ mới:", contact);
+      // gọi API thêm mới ở đây
+      // this.$router.push({ name: "contactbook" });
+    },
+  },
+};
+</script>
